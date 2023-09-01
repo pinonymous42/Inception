10311d3817cb325eaf7b78baa3e78c3a4ff8a0d4
pinonymous42 <matsu1088@keio.jp>
2023-09-01 14:21:59 +0900

    first commit

---
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p /etc/nginx/ssl -p /var/run/nginx
+
+openssl req -newkey rsa:2048 -nodes -x509 -days 3650 \
+        -keyout ${SSL_KEY_PATH} \
+        -out ${SSL_CRT_PATH} \
+        -subj "/C=JP/ST=TOKYO/L=ROPPONGI/O=42 TOKYO/OU=kohmatsu/CN=kohmatsu"
+
+chmod 600 ${SSL_KEY_PATH}
+chmod 600 ${SSL_CRT_PATH}
+nginx -g "daemon off;"
+exec "$@"
---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sleep 5
+wp config create --dbname=${DB_NAME} \
+                 --dbuser=${DB_USER} \
+                 --dbpass=${DB_PASSWORD} \
+                 --dbhost=mariadb \
+                 --path=${WP_PATH} \
+                 --allow-root \
+                 --path="/var/www/html/wordpress"
+
+wp core install --url=${WP_URL} \
+                --title=${WP_TITLE} \
+                --admin_user=${WP_USER} \
+                --admin_password=${WP_PASSWORD} \
+                --admin_email=${WP_EMAIL} \
+                --allow-root \
+                --path="/var/www/html/wordpress"
+/usr/sbin/php-fpm7.3 --nodaemonize
+exec "$@"
